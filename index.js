2590e3496baa399b3c98bc387eb7290f375e275d
govind mall <govind.mall0704@gmail.com>
2024-01-18 14:33:13 +0530

    Json data is given to user

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const app=express()
 
 const port=4060;
 
+const githubData={
+    "name":"Govind Mall",
+    "Roll no":"2001330100115",
+    "College":"NIET"
+}
+
 app.get('/Anuj',(req,res)=>{
     res.send("You are on Home page");
 })
 
 app.get('/twitter',(req,res)=>{
     res.send("Govind Mall on Twitter")
+})
+
+app.get('/userData',(req,res)=>{
+    res.json(githubData);
 })
 
 app.listen(process.env.PORT,()=>{
